9caf30a23f95b57e28ed91bf24355a079d1f90ce
Chris Blanquera <chris@incept.asia>
2025-04-05 14:10:15 +0800

    using frui external

--- frui/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * as FieldType from './field';
-export * as FormatType from './format';
-export * from './element/Alert';
-export * from './element/Badge';
-export * from './form/Button';
-export * from './form/Fieldset';
-export * from './element/Loader';
-export * from './element/Modal';
-export *  from './element/Table';
