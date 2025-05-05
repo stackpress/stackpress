b462010cb1d4e83f23538c4185b84343e82f6814
Chris Blanquera <chris@incept.asia>
2025-05-05 22:37:19 +0800

    exporting types from terminal

--- stackpress/src/terminal/index.ts
+++ stackpress/src/terminal/index.ts
@@ -4,4 +4,6 @@ import Transformer from '@stackpress/idea-transformer/Transformer';
 //terminal
 import Terminal from './Terminal.js';
 
+export type { CLIConfig, CLIPlugin } from './types.js';
+
 export { Terminal, Transformer, control };
