c5275d9bd34707867872f145c73fd8ae984cad00
Chris Blanquera <chris@incept.asia>
2025-05-07 18:33:38 +0800

    removing transformer, now you need to manually add @stackpress/idea-transformer

--- stackpress/src/terminal/index.ts
+++ stackpress/src/terminal/index.ts
@@ -1,9 +1,8 @@
 //stackpress
 import { control } from '@stackpress/lib/Terminal';
-import Transformer from '@stackpress/idea-transformer/Transformer';
 //terminal
 import Terminal from './Terminal.js';
 
 export type { CLIConfig, CLIPlugin } from './types.js';
 
-export { Terminal, Transformer, control };
+export { Terminal, control };
