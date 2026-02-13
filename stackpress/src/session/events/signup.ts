fb7741ebed546a068d9b8fba08c4dade0519065b
cdsiats <118641404+cdsiats@users.noreply.github.com>
2026-02-13 23:02:17 +0800

    corrected path for searching roles in the config #18 1m

--- stackpress/src/session/events/signup.ts
+++ stackpress/src/session/events/signup.ts
@@ -15,7 +15,7 @@ export default async function AuthSignup(
   ctx: Server
 ) {
   //get the roles from the config
-  const roles = ctx.config.path<string[]>('session.auth.roles', []);
+  const roles = ctx.config.path<string[]>('auth.roles', []);
   //get the database engine 
   const engine = ctx.plugin<DatabasePlugin>('database');
   //get the client
