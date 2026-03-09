a05f163e6a82ef87f7ae964c902b5547c480855b
Chris Blanquera <chris@incept.asia>
2026-03-09 12:03:37 +0800

    removing examples. adding templates

--- examples/with-commonjs/plugins/app/pages/home.ts
+++
@@ -1,7 +0,0 @@
-//stackpress
-import { action } from 'stackpress/server';
-
-export default action(async function ErrorPage(req, res) {
-  const name = req.data.path('name', 'guest');
-  res.setHTML(`<h1>Hello ${name}</h1>`);
-});
---
+++ templates/website/plugins/store/populate.ts
@@ -0,0 +1,26 @@
+//stackpress
+import type { ProfileAuth } from 'stackpress';
+import { action } from 'stackpress/server';
+
+const secret = process.env.ADMIN_PASS || 'admin';
+
+export default action(async function Populate(_req, _res, ctx) {
+  const admin = await ctx.resolve<ProfileAuth>('auth-signup', {
+    type: 'person',
+    name: 'Admin',
+    username: 'admin',
+    email: 'admin@project.com',
+    secret: secret,
+    roles: [ 'ADMIN' ]
+  });
+  if (admin.code !== 200) {
+    console.log('Error creating admin user', admin);
+    return;
+  }
+  await ctx.resolve('application-create', {
+    profileId: admin.results?.id,
+    name: 'Example App',
+    scopes: [ 'profile-write', 'auth-read' ],
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365)
+  });
+});
