8dc927f4f6017ab314e2b33b0d8709c2486f7de7
Chris Blanquera <chris@incept.asia>
2026-04-20 22:10:36 +0800

    language plugin

---
+++ packages/stackpress-language/src/index.ts
@@ -0,0 +1,12 @@
+import Language from './Language.js';
+
+export type {
+  Scalar,
+  Language as LanguageData,
+  LanguageMap,
+  LanguageConstructor,
+  LanguageConfig,
+  LanguagePlugin
+} from './types.js';
+
+export { Language };
