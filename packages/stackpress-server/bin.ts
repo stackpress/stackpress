5ef544ab835a953798204a498787efe8ef3a44a8
Joseph Louise <josephlouise0909@gmail.com>
2026-07-13 15:01:42 +0800

    Stackpress/blog client test runners

--- packages/stackpress-server/bin.ts
+++ packages/stackpress-server/bin.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env tsx
 import path from 'node:path';
-import { pathToFileURL } from 'node:url';
 import { objectFromArgs, isObject } from '@stackpress/lib/Nest';
 import { server as http } from '@stackpress/ingest/http';
 import Terminal from 'stackpress-server/Terminal';
@@ -38,7 +37,7 @@ async function getBootstrap(args: string[]) {
   //determine bootstrap pathname
   const bootstrap = getPathname(args);
   //import bootstrap file
-  const imports = bootstrap ? await import(pathToFileURL(bootstrap).href) : {};
+  const imports = bootstrap ? await import(bootstrap) : {};
   //we just need the default export...
   return imports.default || {};
 }
